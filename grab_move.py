65dd943671f383cc905aea2b9a0bcf62ff715e92
Stefan Swaans <sswaans@stanford.edu>
2017-06-05 18:38:59 -0700

    finished documenting and adding features

--- grab_move.py
+++ grab_move.py
@@ -5,56 +5,133 @@ import vizshape
 import vizact
 
 import steamvr
+'''
+Grab module by Khoi Le 2017 - Stanford Virtual Human Interaction Lab
 
+Initialize grab module with the following parameters:
+Necessary:
+- Items to grab, list of node3d objects
+- Controllers, list of SteamVR controllers
+- Destinations to put the objects, list of node3d objects
+
+Pre-assigned parameters:
+- itemsInHand, [] list of node3d objects that appear in hand.
+	IMPORTANT: Order of list corresponds to items. (i.e [object1, object2] [itemInHand1, itemInHand2])
+	itemInHand1 appears when object1 is grabbed. Do NOT pass in lists of different lengths.
+- itemsAtDestination, [] list of node3d objects that appear when object placed destination.
+	IMPORTANT: Order of list corresponds to destinations. (i.e [destination1, destination2] [itemAtDst1, itemAtDst2])
+	itemAtDst1 appears at destination1 when destination1 is touched. Do NOT pass in lists of different lengths.
+- oneItemOneDest, False, boolean determines if each item can only go to its corresponding destination
+	(i.e blue ball goes in blue basket)
+	IMPORTANT: Order of destinations list must correspond to items list.
+		Do NOT pass in lists of different lengths if this is True.
+- hideDestination, True, boolean determines whether the destination should be hidden after placing object
+- regrab, False, boolean determines if participant can regrab an item they've already put down
+- reqTrigger, False, boolean determines if participant needs to pull trigger to initialize grab. NOTE: participants
+	do NOT need to hold the trigger to maintain grip (this would make it possible to drop objects, which is annoying
+	to deal with in an experiment, and should be avoided unless necessary)
+- growOnHover, True, boolean determines if hovering over an object enlarges it for user experience cues when reqTrigger is on.
+- handSensitivity, double that represents the radius of the sphere around the hand
+Examples:
+BASIC TOUCH TO GRAB AND TOUCH DESTINATION TO RELEASE
+
+box = viz.addChild('box.wrl')
+box.setPosition([0, 1, 0])
+box.setScale([.1,.1,.1])
+items = [box]
+dstbox = viz.addChild('box.wrl')
+dstbox.setPosition([1, 1, 0])
+dstbox.setScale([.05,.05,.05])
+dstbox2 = viz.addChild('box.wrl')
+dstbox2.setPosition([.3, 1, 0.3])
+dstbox2.setScale([.05,.05,.05])
+destinations = [dstbox, dstbox2]
+
+grabMod = grab_move.grab_mod(items, controllers, destinations)
+
+See sample testGrab.py for more examples.
+'''
+def toggleView(node, state):
+	'Helper function makes sure items disappear even in mirrors'
+	if state:
+		node.enable(viz.RENDERING)
+		node.alpha(1)
+	else:
+		node.disable(viz.RENDERING)
+		node.alpha(0)
+		
 class grab_mod:
 	'Handles all grabbing interactions'
-	def __init__(self, items, controllers, destinations, reqTrigger = False, itemsInHand = [], handSensitivity = 0.12):
-		#Items - list of node3d objects
-		#Targets - list of node3d objects or locations
+	def __init__(self, items, controllers, destinations, itemsInHand = [], itemsAtDestination = [], oneItemOneDest = False, hideDestination = True, regrab = False, reqTrigger = False, growOnHover = True, handSensitivity = 0.09):
 		self.items = items
 		self.itemsInHand = itemsInHand
 		self.controllers = controllers
 		self.destinations = destinations
+		self.itemsAtDestination = itemsAtDestination
 		self.reqTrigger = reqTrigger
 		self.grabProximityManager = vizproximity.Manager()
 		self.placeProximityManager = vizproximity.Manager()
 		self.grabbedItems = []
+		self.regrab = regrab
+		self.growOnHover = growOnHover
 		self.handSensitivity = handSensitivity
 		self.hoveredItem = None
+		self.oneItemOneDest = oneItemOneDest
+		self.hideDestination = hideDestination
 		vizact.onkeydown('d', self.grabProximityManager.setDebug, viz.TOGGLE)
 		self.grabProximityManager.onEnter(None,self.grabItem)
 		self.grabProximityManager.onExit(None, self.exitNode)
 		self.placeProximityManager.onEnter(None, self.placeItem)
 		self.targets = [None, None]
-		#viztask.schedule(self.grabItem)
+		#Initialize all items
 		for i in range(0, len(items)):
-			items[i] = itemToGrab(items[i])
+			# If module will show special items in hand, link them to the itemToGrab objects
+			if len(itemsInHand) > 0:
+				if self.oneItemOneDest:
+					items[i] = itemToGrab(items[i], itemsInHand[i], destinations[i])
+				else:
+					items[i] = itemToGrab(items[i], itemsInHand[i])
+			else:
+				if self.oneItemOneDest:
+					items[i] = itemToGrab(items[i], destinations[i])
+				else:
+					items[i] = itemToGrab(items[i])
 			items[i].setup(self.grabProximityManager)
+		#Initialize controllers and targets
 		for i in range(len(controllers)):
 			targetSphere = vizshape.addSphere(handSensitivity)
-			targetSphere.disable(viz.RENDERING)
+			toggleView(targetSphere, False)
 			viz.link(controllers[i], targetSphere)
 			target = vizproximity.Target(targetSphere)
 			self.grabProximityManager.addTarget(target)
 			self.placeProximityManager.addTarget(target)
 			self.targets[i] = target
-
+		#Initialize destinations
 		for destination in destinations:
 			destination = itemToGrab(destination)
 			destination.setup(self.placeProximityManager, sensorScale = [3,3,3])
-			
+		
 		for item in itemsInHand:
-			item.disable(viz.RENDERING)
+			toggleView(item, False)
+			
+		for item in itemsAtDestination:
+			toggleView(item, False)
 			
 		vizact.onsensordown(controllers[0], steamvr.BUTTON_TRIGGER, self.triggerGrabItem, 0)
 		vizact.onsensordown(controllers[1], steamvr.BUTTON_TRIGGER, self.triggerGrabItem, 1)
 	def exitNode(self, e):
+		if(self.hoveredItem is not None and self.reqTrigger and self.growOnHover):
+			tempScale = self.hoveredItem.getScale()
+			self.hoveredItem.setScale(tempScale[0] * 0.95238, tempScale[1] * 0.95238, tempScale[2] * 0.95238)
 		self.hoveredItem = None
 	def triggerGrabItem(self, controllerIndex):
 		print("trigger grab called")
 		if self.hoveredItem == None or not self.reqTrigger:
 			return
 		else:
+			if self.growOnHover:
+				tempScale = self.hoveredItem.getScale()
+				self.hoveredItem.setScale(tempScale[0] * 0.95238, tempScale[1] * 0.95238, tempScale[2] * 0.95238)
 			target = self.targets[controllerIndex]
 			self.actuallyGrabItem(self.hoveredItem, target.getSource())
 			
@@ -67,36 +144,78 @@ class grab_mod:
 		
 	def grabItem(self, e):
 		#viztask.schedule(self.actuallyGrabItem(e))
+		for i in range(len(self.items)):
+			if e.sensor.getSource()._node == self.items[i].itemNode:
+				if self.items[i].recentlyGrabbed == True:
+					return;
+		#Vibrate correct controller
+		if e.target == self.targets[0]:
+			print("Vibrate 0")
+			self.vibrateController(0)
+		else:
+			print("Vibrate 1")
+			self.vibrateController(1)
 		self.hoveredItem = e.sensor.getSource()._node
-		if not self.reqTrigger:			
+		if self.reqTrigger and self.growOnHover:
+			#Enlarges object for visual cue
+			tempScale = self.hoveredItem.getScale()
+			self.hoveredItem.setScale(tempScale[0] * 1.05, tempScale[1] * 1.05, tempScale[2] * 1.05)
+		else:			
 			self.actuallyGrabItem(e.sensor.getSource()._node, e.target.getSource());
 		
 	def actuallyGrabItem(self, item, controller):
-		if len(self.itemsInHand) > 0:
-			for i in range(len(items)):
-				if item == self.items[i].item:
-					self.itemsInHand[i].enable(viz.RENDERING)
-					item.disable(viz.RENDERING)
-					link = viz.link(controller._node, self.itemsInHand[i])
-					self.grabbedItems.append([item._node, controller, link])
+		if controller == self.targets[0].getSource():
+			print("Vibrate 0")
+			self.vibrateController(0)
 		else:
-			itemGrabbed = item
-			link = viz.link(controller._node,itemGrabbed)
-			self.grabbedItems.append([itemGrabbed, controller, link]) #make into a class
+			print("Vibrate 1")
+			self.vibrateController(1)
+		if len(self.itemsInHand) > 0:
+			for i in range(len(self.items)):
+				if item == self.items[i].itemNode:
+					toggleView(self.itemsInHand[i], True)
+					toggleView(item, False)
+					link = viz.link(controller._node, self.itemsInHand[i])
+					self.grabbedItems.append([self.items[i], controller, link])
+		else:
+			for i in range(len(self.items)):
+				if item == self.items[i].itemNode:
+					link = viz.link(controller._node,item)
+					self.grabbedItems.append([self.items[i], controller, link]) #make into a class
 		
 	def placeItem(self, e):
 		print(self.grabbedItems)
-		target = e.target.getSource()		
+		target = e.target.getSource()
+		if self.oneItemOneDest and not e.sensor.getSource()._node == grabbedItem[0].specificTarget:
+			return
+		if target == self.targets[0].getSource():
+			print("Vibrate 0")
+			self.vibrateController(0)
+		else:
+			print("Vibrate 1")
+			self.vibrateController(1)
 		for grabbedItem in self.grabbedItems:
 			if target == grabbedItem[1]:
+				
 				if len(self.itemsInHand) > 0:
-					for i in range(len(items)):
-						if grabbedItem[0] == self.items[i].item:
-							self.destinations[i].enable(viz.RENDERING)
-							self.itemsInHand[i].disable(viz.RENDERING)
-							self.grabbedItems.remove(grabbedItem)
+					for i in range(len(self.destinations)):
+						if e.sensor.getSource()._node == self.destinations[i]:
+							if len(self.itemsAtDestination) > 0:
+								print("show new obj")
+								if self.hideDestination:
+									toggleView(self.destinations[i], False)
+								toggleView(self.itemsAtDestination[i], True)
+								toggleView(grabbedItem[0].itemInHand, False) #self.itemsInHand[i].disable(viz.RENDERING)
+							else:
+								if self.hideDestination:
+									toggleView(self.destinations[i], False)
+								else:
+									toggleView(self.destinations[i], True)
+				if not self.regrab:
+					grabbedItem[0].destroySensor()
+				grabbedItem[0].callSRG()
 				grabbedItem[2].remove()
-				grabbedItem[0].setPosition(e.sensor.getSource()._node.getPosition())
+				grabbedItem[0].itemNode.setPosition(e.sensor.getSource()._node.getPosition())
 				self.grabbedItems.remove(grabbedItem)
 				
 	def vibrateController(self, i):
@@ -106,8 +225,13 @@ class grab_mod:
 
 class itemToGrab:
 	'Base class for an item to grab'
-	def __init__(self, itemNode):
+	def __init__(self, itemNode, itemInHand = None, specificTarget = None):
 		self.itemNode = itemNode
+		self.recentlyGrabbed = False
+		self.itemInHand = itemInHand
+		self.sensor = None
+		self.specificTarget = None
+		self.proximityManagers = []
 		#self.targetNode = targetNode
 	
 	def setup(self, proximityManager, sensorType = "box", sensorScale = [1, 1, 1], target = None, itemInHand = None):
@@ -119,6 +243,22 @@ class itemToGrab:
 		else:
 			print("Sensor type: " + sensorType + " is not valid. Defaulting to box sensor. ")
 			sensor = vizproximity.addBoundingBoxSensor(self.itemNode)#, scale=sensorScale)
-		
+		self.sensor = sensor
+		#viztask.schedule(self.setRecentlyGrabbed)
 		proximityManager.addSensor(sensor)
+		self.proximityManagers.append(proximityManager)
+		
+	def callSRG(self):
+		viztask.schedule(self.setRecentlyGrabbed)
+		
+	def setRecentlyGrabbed(self):
+		self.recentlyGrabbed = True
+		print("grabbed true")
+		yield viztask.waitTime(0.5)
+		self.recentlyGrabbed = False
+		print("grabbed false")
+	
+	def destroySensor(self):
+		for manager in self.proximityManagers:
+			manager.removeSensor(self.sensor)
 		
--- testGrab.py
+++ testGrab.py
@@ -6,6 +6,11 @@ import steamvr
 
 import grab_move
 
+''' THIS IS THE DOCUMENTATION AND SAMPLE SCRIPTS FOR grab_move.py 
+	KHOI LE 2017 - Stanford Virtual Human Interaction Lab
+'''
+
+# Set up testing environment and SteamVR
 viz.go()
 
 hmd = steamvr.HMD()
@@ -15,10 +20,50 @@ viewLink.preMultLinkable(hmd.getSensor())
 
 viz.addChild('piazza.osgb')
 
-controllers = steamvr.getControllerList()
+controllers = steamvr.getControllerList() # The controller list to pass
 assert(len(controllers) == 2)
 viz.link(controllers[0],controllers[0].addModel()) # = viz.addChild('box.wrl')
 viz.link(controllers[1],controllers[1].addModel()) # = viz.addChild('box.wrl')
+
+''' SOAE SNAIL HUNT EXAMPLE PSEUDOCODE '''
+'''
+basket = basket object to grab flags from
+flag = flag object to appear in hand
+snails = list of snails in scene [snail1, snail2, snail3]
+flagsAtSnails = list of flags on top of specific snails [flagThatGoesOnSnail1, flagThatGoesOnSnail2, flagThatGoesOnSnail3]
+controllers = steamvr.getControllerList()
+
+snailHuntGrabMod = grab_move.grab_mod([basket], controllers, snails, [flag], flagsAtSnails, hideDestination = False)
+#if you want participant to pull trigger to grab a flag from basket, set reqTrigger = True
+
+We pass in [basket] as a list of items where we grab from
+We pass in our steamvr controllers
+We pass in snails as the list of destinations where we want to put the objects
+We pass in [flag] as a list of items to show up in our hand when we grab the basket
+We pass in flagsAtSnails as the list of items at the destinations, which will show up at the corresponding
+	destination when we place the item at that destination
+We set hideDestination to False because we don't want the snails to disappear when we place the flag there.
+'''
+''' DARPA UNPACKING EXAMPLE PSEUDOCODE '''
+'''
+itemsInSuitcase = objects in suitcase [shampoo, conditioner]
+itemsInHand = objects in hand [shampooHand, conditionerHand]
+itemsOnShelf = materialized items on shelf [shampooShelf, conditionerShelf]
+itemsShelfPlaceholder = ghosty items on shelf [shampooGhosty, conditionerGhosty]
+controllers = steamvr.getControllerList()
+unpackingGrabMod = grab_move.grab_mod(itemsInSuitcase, controllers, itemsShelfPlaceholder, itemsInHand, itemsOnShelf, oneItemOneDest = True)
+
+We pass in itemsInSuitcase as the list of objects we want participant to grab
+We pass in our steamvr controllers
+We pass in itemsShelfPlaceholder as the list of destinations where we want our participant to put the objects
+We pass in itemsInHand as the list of objects we want to be in participant hand
+We pass in itemsOnShelf as the list of items at the destination which will appear when we place the object
+	at the correct destination
+We set oneItemOneDest to True because want each item to go to a specific spot
+'''
+
+''' BASIC TOUCH TO GRAB AND TOUCH DESTINATION TO RELEASE '''
+#'''
 box = viz.addChild('box.wrl')
 box.setPosition([0, 1, 0])
 box.setScale([.1,.1,.1])
@@ -31,4 +76,87 @@ dstbox2.setPosition([.3, 1, 0.3])
 dstbox2.setScale([.05,.05,.05])
 destinations = [dstbox, dstbox2]
 
-grabMod = grab_move.grab_mod(items, controllers, destinations, True)
+grabMod = grab_move.grab_mod(items, controllers, destinations) 
+#'''
+
+''' PULL TRIGGER TO GRAB AND TOUCH DESTINATION TO RELEASE '''
+'''
+box = viz.addChild('box.wrl')
+box.setPosition([0, 1, 0])
+box.setScale([.1,.1,.1])
+items = [box]
+dstbox = viz.addChild('box.wrl')
+dstbox.setPosition([1, 1, 0])
+dstbox.setScale([.05,.05,.05])
+dstbox2 = viz.addChild('box.wrl')
+dstbox2.setPosition([.3, 1, 0.3])
+dstbox2.setScale([.05,.05,.05])
+destinations = [dstbox, dstbox2]
+
+grabMod = grab_move.grab_mod(items, controllers, destinations, reqTrigger = True) 
+'''
+
+''' BASIC TOUCH TO GRAB AND TOUCH DESTINATION TO RELEASE WITH REGRAB '''
+'''
+box = viz.addChild('box.wrl')
+box.setPosition([0, 1, 0])
+box.setScale([.1,.1,.1])
+items = [box]
+dstbox = viz.addChild('box.wrl')
+dstbox.setPosition([1, 1, 0])
+dstbox.setScale([.05,.05,.05])
+dstbox2 = viz.addChild('box.wrl')
+dstbox2.setPosition([.3, 1, 0.3])
+dstbox2.setScale([.05,.05,.05])
+destinations = [dstbox, dstbox2]
+
+grabMod = grab_move.grab_mod(items, controllers, destinations, regrab = True) 
+'''
+
+''' TOUCH TO GRAB, OBJECT APPEARS IN HAND, TOUCH DESTINATION TO RELEASE '''
+'''
+box = viz.addChild('box.wrl')
+duck = viz.addChild('duck.wrl')
+duck.setPosition([0.5, 1, 0.5])
+duck.setScale([0.2,0.2,0.2])
+box.setPosition([0, 1, 0])
+box.setScale([.1,.1,.1])
+items = [box]
+itemsInHand = [duck]
+dstbox = viz.addChild('box.wrl')
+dstbox.setPosition([1, 1, 0])
+dstbox.setScale([.05,.05,.05])
+dstbox2 = viz.addChild('box.wrl')
+dstbox2.setPosition([.3, 1, 0.3])
+dstbox2.setScale([.05,.05,.05])
+destinations = [dstbox, dstbox2]
+
+grabMod = grab_move.grab_mod(items, controllers, destinations, itemsInHand)
+'''
+
+''' TOUCH TO GRAB, OBJECT APPEARS IN HAND, TOUCH DESTINATION TO RELEASE, NEW ITEM APPEARS AT DESTINATION 
+	I.E DARPA - SOAE '''
+'''
+box = viz.addChild('box.wrl')
+duck = viz.addChild('duck.wrl')
+duck.setPosition([0.5, 1, 0.5])
+duck.setScale([0.2,0.2,0.2])
+box.setPosition([0, 1, 0])
+box.setScale([.1,.1,.1])
+items = [box]
+itemsInHand = [duck]
+dstbox = viz.addChild('box.wrl')
+dstbox.setPosition([1, 1, 0])
+dstbox.setScale([.05,.05,.05])
+dstbox2 = viz.addChild('box.wrl')
+dstbox2.setPosition([.3, 1, 0.3])
+dstbox2.setScale([.05,.05,.05])
+destinations = [dstbox, dstbox2]
+ball = viz.addChild('basketball.osgb')
+ball.setPosition(dstbox.getPosition())
+ball2 = viz.addChild('basketball.osgb')
+ball2.setPosition(dstbox2.getPosition())
+itemsAtDest = [ball, ball2]
+
+grabMod = grab_move.grab_mod(items, controllers, destinations, itemsInHand, itemsAtDest)
+'''
